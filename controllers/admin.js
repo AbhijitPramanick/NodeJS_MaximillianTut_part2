21d3f680e9829fdf8c14feb823537923929f1544
AbhijitPramanick <amanik1995@gmail.com>
2023-12-05 14:03:34 +0530

    Dynamic Routes and Advanced Models Chapter end

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing:false
+    editing: false
   });
 };
 
@@ -17,35 +17,43 @@ exports.postAddProduct = (req, res, next) => {
   product.save();
   res.redirect('/');
 };
+
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-  if(!editMode){
-    return res.redirect("/");
+  if (!editMode) {
+    return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product=>{
-    if(!product){
-      return res.redirect("/");
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
     }
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
-      editing:editMode,
-      product:product
+      editing: editMode,
+      product: product
     });
   });
 };
 
-exports.postEditProduct = (req,res,next) => {
+exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice );
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
   updatedProduct.save();
-  res.redirect("/admin/products");
-}
+  res.redirect('/admin/products');
+};
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('admin/products', {
@@ -56,8 +64,8 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.postDeleteProducts = (req,res,next) =>{
+exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.deleteById(prodId);
-  res.redirect("/admin/products");
-}
+  res.redirect('/admin/products');
+};
